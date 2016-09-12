f6750cfe762add26e52ff39b945d8a7bdf54caec
Xydawn <xycccch@qq.com>
2016-09-12 11:53:48 +0800

    Initial Commit

---
+++ UdpForMac/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  UdpForMac
+//
+//  Created by 曹操 on 16/9/12.
+//  Copyright © 2016年 Xydawn. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ UdpForMac/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  UdpForMac
+//
+//  Created by 曹操 on 16/9/12.
+//  Copyright © 2016年 Xydawn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
